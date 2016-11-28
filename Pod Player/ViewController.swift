c7fe00a1806a015bc9e2a7d0093e2667a0f38de2
Stephen Maitland <s.maitland@me.com>
2016-11-28 19:43:24 +0000

    Initial Commit

---
+++ Pod Player/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pod Player
+//
+//  Created by Stephen on 28/11/2016.
+//  Copyright © 2016 Luminator Technology. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
